925ef0016585b0f69e16a6965d877e3369d65b6a
cole <icsnyder42@gmail.com>
2022-02-21 17:17:50 -0500

    Modified Linux script Added README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# serving-protobuf-with-javalin
+
+## Description
+
+This is a community example of serving Protobuf over a REST API with Javalin. The project contains a pair of custom build scripts to help automate the protocol buffer compilation process.
+
+## Getting Started
+
+To start, clone or fork this repository. Make sure both git and protoc are installed on target system.
+
+### Dependencies
+
+* Developing on MS Windows or Linux**
+* Using git for version control system
+* protoc compiler and git both installed and in path
+
+** Tested on a Debian based distro
+
+## Authors
+
+Cole Snyder AKA The-Funk
+
+## License
+
+This project is unlicensed. Use it however you see fit.
+
+## Acknowledgments
+
+Special thanks to David Åse (tipsy) and the contributors to the always awesome [javalin web framework](https://github.com/tipsy/javalin)
--- protogen.sh
+++ protogen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Building protocol buffers (!Untested Linux script!)"
+echo "Building protocol buffers (Tested on Debian based distro)"
 echo "Dependencies: git and protoc in path"
 
 # Define variables relative to project src directory here
 projName="serving-protobuf-with-javalin"                            # Set your project's name e.g. myproject or myproject/mymodule
-projectRoot="/root/IdeaProjects/$projName/src/main/java"            # Set your project root folder here
+projectRoot="/root/IdeaProjects/$projName/src/main/java"            # Set your project root folder here (I logged in as root, your home directory may be /home/youruser)
 protoSrc="app/model/objects/proto"                                  # Source directory under project root for proto files
 
 # And here's where the magic happens
@@ -19,8 +19,9 @@ then
     then
       echo "compiling: $file"
       fileNameOnly=$(basename "$file")
-      # Note that we use projectRoot for the java_out path because
-      # protoc will automatically generate subdirectories based on the path provided in the .proto file's 'package' directive (when generating Java code)
+      # Note that we use projectRoot for the java_out path because protoc will automatically generate subdirectories based on the path provided in the .proto file's 'package' directive (when generating Java code).
+      # Note that if you installed the protobuf compiler from a Debian based apt repo, you may have an older version, and the proto3 optional keyword may require the experimental_allow included below.
+      # If you intend to use optional I would highly recommend downloading a newer version of the protobuf-compiler and doing a manual dpkg -i or add a backports repo to your /etc/apt/sources.list.
       protoc -I="$projectRoot/$protoSrc" --proto_path="$projectRoot" --java_out="$projectRoot" --experimental_allow_proto3_optional "$fileNameOnly"
     fi
   done <<< "$changed"
