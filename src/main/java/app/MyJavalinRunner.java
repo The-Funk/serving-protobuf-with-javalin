643b26b1b3140fc9c236864620ea91d9af517eeb
cole <icsnyder42@gmail.com>
2022-02-21 18:33:46 -0500

    Simplified example

--- src/main/java/app/MyJavalinRunner.java
+++ src/main/java/app/MyJavalinRunner.java
@@ -1,22 +1,53 @@
 package app;
 
-import app.control.SensorController;
+import app.model.dao.FakeDao;
+import app.model.objects.pojo.SensorData;
 import io.javalin.Javalin;
-import io.javalin.http.staticfiles.Location;
+import io.javalin.core.JavalinConfig;
+import io.javalin.http.Handler;
+
+import static app.model.mappers.RouteMapper.getSensorDataResponse;
 
 public class MyJavalinRunner {
 
     public static void main(String[] args) {
 
-        Javalin app = Javalin.create(config -> {
-            config.addStaticFiles("/view", Location.CLASSPATH);
-            config.enableCorsForAllOrigins();
-            config.addSinglePageRoot("/", "/view/index.html");
-        }).start();
+        Javalin app = Javalin.create(JavalinConfig::enableCorsForAllOrigins).start();
 
-        app.get("/pbuf", SensorController.handleServeProtobuf);
-        app.get("/json", SensorController.handleServeJSON);
+        app.get("/pbuf", handleServeProtobuf);
+        app.get("/json", handleServeJSON);
 
     }
+
+    public static Handler handleServeProtobuf = ctx -> {
+        try {
+            // Here's where you'd get your SensorData from your DB or device, etc.
+            FakeDao dao = new FakeDao();
+            SensorData sData = dao.getSensorDataFromVehicleDB();
+
+            ctx.status(200);
+            ctx.contentType("application/x-protobuf");
+            ctx.result(getSensorDataResponse(sData));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            ctx.status(500);
+        }
+    };
+
+    public static Handler handleServeJSON = ctx -> {
+        try {
+            // Here's where you'd get your SensorData from your DB or device, etc.
+            FakeDao dao = new FakeDao();
+            SensorData sData = dao.getSensorDataFromVehicleDB();
+
+            ctx.status(200);
+            ctx.json(sData);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            ctx.status(500);
+        }
+    };
 
 }
