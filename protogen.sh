fb85d270e084456cfd7004e934ebb34f07059f6e
root <icsnyder42@gmail.com>
2022-02-21 14:56:58 -0500

    tested linux script

--- protogen.sh
+++ protogen.sh
@@ -3,16 +3,15 @@ echo "Building protocol buffers (!Untested Linux script!)"
 echo "Dependencies: git and protoc in path"
 
 # Define variables relative to project src directory here
-user=$(whoami)                                                      # Gets username from system
 projName="serving-protobuf-with-javalin"                            # Set your project's name e.g. myproject or myproject/mymodule
-projectRoot="/home/$user/IdeaProjects/$projName/src/main/java"      # Set your project root folder here
+projectRoot="/root/IdeaProjects/$projName/src/main/java"            # Set your project root folder here
 protoSrc="app/model/objects/proto"                                  # Source directory under project root for proto files
 
 # And here's where the magic happens
-cd "$projectRoot" || echo "Project root doesn't exist" && exit
-changed=$(git diff --name-only --diff-filter=AM)
+cd "$projectRoot" || echo "Project root doesn't exist"
+changed="$(git diff --name-only --diff-filter=AM)"
 
-if [ -n "$changed" ]
+if [[ -n "$changed" ]]
 then
   while read -r file
   do
@@ -22,9 +21,9 @@ then
       fileNameOnly=$(basename "$file")
       # Note that we use projectRoot for the java_out path because
       # protoc will automatically generate subdirectories based on the path provided in the .proto file's 'package' directive (when generating Java code)
-      protoc -I="$projectRoot/$protoSrc" --proto_path="$projectRoot" --java_out="$projectRoot" "$fileNameOnly"
+      protoc -I="$projectRoot/$protoSrc" --proto_path="$projectRoot" --java_out="$projectRoot" --experimental_allow_proto3_optional "$fileNameOnly"
     fi
-  done < <(changed)
+  done <<< "$changed"
 fi
 echo "Finished building protocol buffers" && exit
 #EOF
